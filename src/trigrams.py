352d020e65d4f96760f23ccd4c42fa6f66aa12b8
pasaunders <kaorti@gmail.com>
2016-12-07 18:27:10 -0800

    three tests working, refactored functions for clarity and brevity

--- src/trigrams.py
+++ src/trigrams.py
@@ -1,7 +1,7 @@
 """A program that takes a txt file input and creates new similar new file."""
 
 import io
-from random import randint
+from random import choice, randint
 
 
 def open_file(text_file):
@@ -15,7 +15,7 @@ def open_file(text_file):
 def build_dictionary(story):
     """Build a dictonary for trigram analysis."""
     trigrams_dict = {}
-    for i in range(len(story)):
+    for i in range(len(story) - 2):
         new_key = " ".join(story[i:i + 2])
         if new_key in trigrams_dict:
             trigrams_dict[new_key].append(story[i + 2])
@@ -26,13 +26,14 @@ def build_dictionary(story):
 
 def write_story(words, trigrams_dict):
     """Use trigram dictonary to build a new story."""
-    new_story = trigrams_dict.keys()[randint(0, len(trigrams_dict) - 1)].split(" ")
+    new_story = choice(trigrams_dict.keys()).split(" ")
     while len(new_story) < words:
         if " ".join(new_story[-2:]) in trigrams_dict:
-            new_story.append(trigrams_dict[" ".join(new_story[-2:])])
+            key_number = " ".join(new_story[-2:])
+            new_story += [choice(trigrams_dict[key_number])]
         else:
-            break
-    return new_story
+            new_story += choice(trigrams_dict.keys()).split(" ")
+    return " ".join(new_story)
 
 
 if __name__ == '__main__':
@@ -41,5 +42,3 @@ if __name__ == '__main__':
     print("How many words should your story be?")
     length_choice = int(input("Please enter a number. "))
     write_story(length_choice, build_dictionary(open_file(story_choice)))
-
-
