1440ba8fdabbd5a1baf1638bb05dc829f4ac514f
Rachael Wisecarver <rachael.wisecarver@gmail.com>
2016-12-07 16:42:04 -0800

    refactoring functions based on testing results, writing if name == main function to run the program in the command line.

--- src/trigrams.py
+++ src/trigrams.py
@@ -7,13 +7,12 @@ from random import randint
 def open_file(text_file):
     """Open and read source text file."""
     source_file = io.open(text_file, 'r+')
-    new_words = source_file.read()
-    story = new_words.split('\W\s\d')
+    story = source_file.read().split('\W\s\d')
     source_file.close()
     return story
 
 
-def build_dictionary():
+def build_dictionary(story):
     """Build a dictonary for trigram analysis."""
     trigrams_dict = {}
     for i in range(len(story)):
@@ -25,7 +24,7 @@ def build_dictionary():
     return trigrams_dict
 
 
-def write_story(words):
+def write_story(words, trigrams_dict):
     """Use trigram dictonary to build a new story."""
     new_story = trigrams_dict.keys()[randint(0, len(trigrams_dict) - 1)].split(" ")
     while len(new_story) < words:
@@ -34,4 +33,13 @@ def write_story(words):
         else:
             break
     return new_story
+
+
+if __name__ == '__main__':
+    print("Please pick a story, and I will make another just like it.")
+    story_choice = str(input("Please enter a file path for your story. "))
+    print("How many words should your story be?")
+    length_choice = int(input("Please enter a number. "))
+    write_story(length_choice, build_dictionary(open_file(story_choice)))
+
 
